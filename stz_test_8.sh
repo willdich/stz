fe418e8394bc358f9c1b11a73be8da261bd207be
Anna Whitney <awhitney@college.harvard.edu>
2015-12-05 17:03:51 -0500

    Runscripts for Odyssey

---
+++ stz_test_8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -p general
+#SBATCH -n 8
+#SBATCH -N 1-8
+#SBATCH -t 10
+#SBATCH --mem=100
+
+source new-modules.sh
+module load gcc openmpi
+module load Anaconda
+
+start_dir=$PWD
+mkdir -p /scratch/$USER/$SLURM_JOBID
+cp driver.py /scratch/$USER/$SLURM_JOBID
+cp test.conf /scratch/$USER/$SLURM_JOBID
+cd /scratch/$USER/$SLURM_JOBID
+
+mpiexec -n 8 python driver.py test.conf
+
+cp  * $start_dir/
+cd $start_dir
+
+rm -rf /scratch/$USER/$SLURM_JOBID
