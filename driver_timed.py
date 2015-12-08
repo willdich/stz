5610f55e8ebaa65c097b5a9bad2be8da3028e722
Anna Whitney <awhitney@college.harvard.edu>
2015-12-08 07:47:20 -0500

    Cleanup and added timer code

---
+++ driver_timed.py
@@ -0,0 +1,51 @@
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+#import mpi4py
+#from os import environ
+
+# http://stackoverflow.com/questions/2741399/python-pyximporting-a-pyx-that-depends-on-a-native-library
+# Link to math library without having to write setup.py
+#environ['LDFLAGS'] = '-Lm -lm'
+
+# Automatic Cython file compilation
+import pyximport
+#pyximport.install(setup_args={"include_dirs":[np.get_include(), mpi4py.get_include()]})
+
+from timer import Timer
+from parse_input import parse_input
+from sim import go
+
+def prepare_input(config_file):
+
+    # get simulation inputs from configuration file
+    lambd, mu, rho, min_x, max_x, min_y, max_y, min_z, max_z, N_x, N_y, N_z, t_0, t_f, N_t, outfile = parse_input(config_file)
+
+    # Grid spacing in each dimension
+    # We divide by N_i - 1 so that we include L_x in our boundary
+    dx = (max_x - min_x) / N_x
+    dy = (max_y - min_y) / N_y
+    dz = (max_z - min_z) / N_z
+
+    # Timestep
+    dt = (t_f - t_0) / N_t
+
+    # Grid size in each direction
+    L_x = np.float64(max_x) - np.float64(min_x)
+    L_y = np.float64(max_y) - np.float64(min_y)
+    L_z = np.float64(max_z) - np.float64(min_z)
+
+    return N_x, N_y, N_z, N_t, L_x, L_y, L_z, dx, dy, dz, dt, mu, rho, lambd, t_0, t_f, outfile
+
+
+if __name__ == '__main__':
+    import sys
+
+    config_file = sys.argv[1]
+    opts = prepare_input(config_file)
+
+    with Timer() as t:
+        go(*opts)
+
+    with open('time-' + opts[-1]) as timer_outfile:
+        print(t.interval, file=timer_outfile)
--- test_stz_8.sh
+++ test_stz_8.sh
@@ -9,17 +9,19 @@ source new-modules.sh
 module load gcc openmpi
 module load Anaconda
 
-start_dir=$PWD
-mkdir -p /scratch/$USER/$SLURM_JOBID
-cp *.py /scratch/$USER/$SLURM_JOBID
-cp *.pxd /scratch/$USER/$SLURM_JOBID
-cp *.so /scratch/$USER/$SLURM_JOBID
-cp test.conf /scratch/$USER/$SLURM_JOBID
-cd /scratch/$USER/$SLURM_JOBID
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/n/sw/fasrcsw/apps/Core/Anaconda/1.9.2-fasrc01/x/lib/mpi4py/include/
+
+#start_dir=$PWD
+#mkdir -p /scratch/$USER/$SLURM_JOBID
+#cp *.py /scratch/$USER/$SLURM_JOBID
+#cp *.pxd /scratch/$USER/$SLURM_JOBID
+#cp *.so /scratch/$USER/$SLURM_JOBID
+#cp test.conf /scratch/$USER/$SLURM_JOBID
+#cd /scratch/$USER/$SLURM_JOBID
 
 mpiexec -n 8 python driver.py test.conf
 
-cp *.out *.dat $start_dir/
-cd $start_dir
+#cp *.out *.dat $start_dir/
+#cd $start_dir
 
-rm -rf /scratch/$USER/$SLURM_JOBID
+#rm -rf /scratch/$USER/$SLURM_JOBID
