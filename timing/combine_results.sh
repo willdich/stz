20150a533400463e741817b59041efe08ec6fb84
Anna Whitney <awhitney@college.harvard.edu>
2015-12-10 08:50:57 -0500

    Completed timing for smaller problem sizes

--- timing/combine_results.sh
+++ timing/combine_results.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-for n in 16 32 64
-do
-    for p in 1 2 4 8
-    do
-        cat time-${p}_${n}.out >> all-times-$n
-    done
-done
-
-for n in 128 256
+for n in 16 32 64 128 256
 do
     for p in 1 2 4 8 16 32 64
     do
